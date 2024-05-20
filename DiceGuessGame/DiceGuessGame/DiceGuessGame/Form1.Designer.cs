1ebaff43ea466816a500a85712c32391e95cd921
Nicholas Krivanec <168858569+nicholaskrivanec@users.noreply.github.com>
2024-05-20 13:44:58 -0600

    Add files via upload

---
+++ DiceGuessGame/DiceGuessGame/DiceGuessGame/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace DiceGuessGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.diceImage = new System.Windows.Forms.PictureBox();
+            this.RollButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.statsBox = new System.Windows.Forms.RichTextBox();
+            this.GameInfoGroupBox = new System.Windows.Forms.GroupBox();
+            this.amtLost = new System.Windows.Forms.Label();
+            this.amtWon = new System.Windows.Forms.Label();
+            this.amtPlayed = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.guessLabel = new System.Windows.Forms.Label();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.noInputMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.diceImage)).BeginInit();
+            this.GameInfoGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // diceImage
+            // 
+            this.diceImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.diceImage.Image = global::DiceGuessGame.Properties.Resources.die6;
+            this.diceImage.InitialImage = null;
+            this.diceImage.Location = new System.Drawing.Point(51, 218);
+            this.diceImage.Name = "diceImage";
+            this.diceImage.Size = new System.Drawing.Size(76, 71);
+            this.diceImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.diceImage.TabIndex = 0;
+            this.diceImage.TabStop = false;
+            // 
+            // RollButton
+            // 
+            this.RollButton.Location = new System.Drawing.Point(180, 218);
+            this.RollButton.Name = "RollButton";
+            this.RollButton.Size = new System.Drawing.Size(75, 23);
+            this.RollButton.TabIndex = 1;
+            this.RollButton.Text = "Roll";
+            this.RollButton.UseVisualStyleBackColor = true;
+            this.RollButton.Click += new System.EventHandler(this.RollButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(180, 266);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 2;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // statsBox
+            // 
+            this.statsBox.BackColor = System.Drawing.SystemColors.Window;
+            this.statsBox.Location = new System.Drawing.Point(13, 315);
+            this.statsBox.Name = "statsBox";
+            this.statsBox.ReadOnly = true;
+            this.statsBox.Size = new System.Drawing.Size(389, 105);
+            this.statsBox.TabIndex = 3;
+            this.statsBox.Text = resources.GetString("statsBox.Text");
+            // 
+            // GameInfoGroupBox
+            // 
+            this.GameInfoGroupBox.Controls.Add(this.amtLost);
+            this.GameInfoGroupBox.Controls.Add(this.amtWon);
+            this.GameInfoGroupBox.Controls.Add(this.amtPlayed);
+            this.GameInfoGroupBox.Controls.Add(this.label3);
+            this.GameInfoGroupBox.Controls.Add(this.label2);
+            this.GameInfoGroupBox.Controls.Add(this.label1);
+            this.GameInfoGroupBox.Location = new System.Drawing.Point(16, 12);
+            this.GameInfoGroupBox.Name = "GameInfoGroupBox";
+            this.GameInfoGroupBox.Size = new System.Drawing.Size(239, 143);
+            this.GameInfoGroupBox.TabIndex = 4;
+            this.GameInfoGroupBox.TabStop = false;
+            this.GameInfoGroupBox.Text = "Game Info";
+            // 
+            // amtLost
+            // 
+            this.amtLost.AutoSize = true;
+            this.amtLost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amtLost.Location = new System.Drawing.Point(203, 107);
+            this.amtLost.Name = "amtLost";
+            this.amtLost.Size = new System.Drawing.Size(16, 16);
+            this.amtLost.TabIndex = 5;
+            this.amtLost.Text = "0";
+            // 
+            // amtWon
+            // 
+            this.amtWon.AutoSize = true;
+            this.amtWon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amtWon.Location = new System.Drawing.Point(203, 63);
+            this.amtWon.Name = "amtWon";
+            this.amtWon.Size = new System.Drawing.Size(16, 16);
+            this.amtWon.TabIndex = 4;
+            this.amtWon.Text = "0";
+            // 
+            // amtPlayed
+            // 
+            this.amtPlayed.AutoSize = true;
+            this.amtPlayed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amtPlayed.Location = new System.Drawing.Point(203, 20);
+            this.amtPlayed.Name = "amtPlayed";
+            this.amtPlayed.Size = new System.Drawing.Size(16, 16);
+            this.amtPlayed.TabIndex = 3;
+            this.amtPlayed.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Times Lost:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Times Won:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Times Played:";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(192, 175);
+            this.TextBox1.MaxLength = 1;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(29, 20);
+            this.TextBox1.TabIndex = 5;
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.TextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // guessLabel
+            // 
+            this.guessLabel.AutoSize = true;
+            this.guessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessLabel.Location = new System.Drawing.Point(23, 179);
+            this.guessLabel.Name = "guessLabel";
+            this.guessLabel.Size = new System.Drawing.Size(163, 16);
+            this.guessLabel.TabIndex = 6;
+            this.guessLabel.Text = "Enter your guess (1-6):";
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.BackColor = System.Drawing.SystemColors.Info;
+            this.errorMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.errorMessage.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.errorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorMessage.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.errorMessage.Location = new System.Drawing.Point(227, 181);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(207, 15);
+            this.errorMessage.TabIndex = 7;
+            this.errorMessage.Text = "Only numbers 1 through 6 are valid";
+            this.errorMessage.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.errorMessage.Visible = false;
+            // 
+            // noInputMessage
+            // 
+            this.noInputMessage.AutoSize = true;
+            this.noInputMessage.BackColor = System.Drawing.SystemColors.Info;
+            this.noInputMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noInputMessage.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.noInputMessage.Location = new System.Drawing.Point(227, 168);
+            this.noInputMessage.Name = "noInputMessage";
+            this.noInputMessage.Size = new System.Drawing.Size(166, 13);
+            this.noInputMessage.TabIndex = 8;
+            this.noInputMessage.Text = "<-Enter a guess before Roll.";
+            this.noInputMessage.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(444, 446);
+            this.Controls.Add(this.noInputMessage);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.guessLabel);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.GameInfoGroupBox);
+            this.Controls.Add(this.statsBox);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.RollButton);
+            this.Controls.Add(this.diceImage);
+            this.Name = "Form1";
+            this.Text = "Die Guess Game";
+            this.Enter += new System.EventHandler(this.RollButton_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.diceImage)).EndInit();
+            this.GameInfoGroupBox.ResumeLayout(false);
+            this.GameInfoGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox diceImage;
+        private System.Windows.Forms.Button RollButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.RichTextBox statsBox;
+        private System.Windows.Forms.GroupBox GameInfoGroupBox;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label guessLabel;
+        private System.Windows.Forms.Label errorMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label amtLost;
+        private System.Windows.Forms.Label amtWon;
+        private System.Windows.Forms.Label amtPlayed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label noInputMessage;
+    }
+}
+
---
+++ DiceGuessGame/DiceGuessGame/DiceGuessGame/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceGuessGame
+{
+    public partial class Form1 : Form
+    {
+        #region Attributes:
+        // constants to define array sizes
+        public const int NUM_DICE = 6; // represents the number of dice face values
+        public const int NUM_ROWS = 7; // number of rows in the statBox
+        public const int NUM_COLS = 4; // number of columns in the statBox
+
+        // stores the user's guess (1-6) and the dice roll value
+        private int guess;
+        private int randomInt;
+
+        // to get random dice roll:
+        private Random iRandom = new Random();
+
+        // boolean shows if guess is valid input
+        private bool isValid;
+
+        // amount of Wins, and losses; appearing in the Game Info box: timesPlayed is the their sum
+        private int iTimesWon;
+        private int iTimesLost;
+        private int iTimesPlayed;
+
+        // Each Column's data going in the statBox
+        private int[] iArrFrequency;
+        private double[] dArrPercent;
+        private int[] iArrGuesses;
+
+        // spacing before columns in table
+        private const string sFaceSpace = "   ";
+        private const string sFreqSpace = "        ";
+        private const string sPercSpace = "    ";
+
+        // tabs after columns in stat table
+        private const string sFaceTab = "\t";
+        private const string sFreqTab = "\t\t";
+        private const string sPercTab = "\t\t\t";
+
+        // 2D array to hold stat string values to go in stat rich textbox
+        private string[,] statTable;
+
+        // define the column names as constants
+        private const int FACE = 0;
+        private const int FREQ = 1;
+        private const int PERC = 2;
+        private const int GUES = 3;
+        #endregion
+
+        #region methods:
+        /// <summary>
+        /// Initializes Component along with most variables.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            iTimesWon = 0;
+            iTimesLost = 0;
+            iTimesPlayed = 0;
+            isValid = false;
+            iArrFrequency = new int[NUM_DICE] { 0, 0, 0, 0, 0, 0 };
+            dArrPercent = new double[NUM_DICE] { 0.00, 0.00, 0.00, 0.00, 0.00, 0.00 };
+            iArrGuesses = new int[NUM_DICE] { 0, 0, 0, 0, 0, 0 };
+
+            // initialize the stat table
+            statTable = new string[NUM_ROWS, NUM_COLS]
+            {
+                //spaces before value, tabs after
+                {"FACE\t",         "FREQUENCY\t",       "PERCENT\t",    "NUMBER OF GUESSES\n" },
+                {sFaceSpace+"1"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"2"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"3"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"4"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"5"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"6"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0" }
+            };
+
+        }
+
+        /// <summary>
+        /// Changes the dice image using random int when RollButton is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+            // hide all error messages
+            HideErrorMessage();
+            HideInputNeededMessage();
+
+            // if they haven't entered a guess, display enter guess error message
+            if (TextBox1.Text == "")
+            {
+                ShowInputNeededMessage();
+                return;
+            }
+            
+            // if input is invalid show message and return
+            if (isValid == false)
+            {
+                ShowErrorMessage();
+                return;
+            }
+
+            AnimateDice();                                                              // simulate the dice roll
+            randomInt = iRandom.Next(1, 7);                                             // store the random int
+            diceImage.Image = Image.FromFile("die" + randomInt.ToString() + ".gif");    // retrieve the new Dice image file using random int
+            diceImage.Refresh();                                                        // refresh the dice image
+
+            UpdateValues(guess, randomInt);    // update attribute values
+            UpdateGameInfo();                  // update the game info box
+            UpdateGameStatsBox();              // update the game stats textbox
+        }
+
+        /// <summary>
+        /// updates all game values: wins, losses, etc...; does NOT change displays of value
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private void UpdateValues(int iGuess, int iRandom)
+        {
+            // if guess is the same as dice roll, timesWon++, else timesLost++
+            if (iGuess == iRandom)
+            {
+                iTimesWon++;
+            }
+            else
+            {
+                iTimesLost++;
+            }
+     
+            // increment Number of Guesses and update statTable    
+            iArrGuesses[iGuess - 1]++;
+            statTable[iGuess, GUES] = iArrGuesses[iGuess - 1].ToString();
+
+            // if guess was not 6 then add a newline at the number guessed
+            if (iGuess != 6)
+            {
+                statTable[iGuess, GUES] += '\n';
+            }
+
+            // increment roll Frequency and update statTable
+            iArrFrequency[iRandom - 1]++;
+            statTable[iRandom, FREQ] = sFreqSpace + iArrFrequency[iRandom - 1].ToString()+sFreqTab;
+
+            iTimesPlayed = iTimesWon + iTimesLost;
+
+            // update the percentages using frequency/timesplayed
+            for (int i = 0; i < NUM_DICE; i++)
+            {
+                // update the actual percentage doubles, then the stat table
+                dArrPercent[i] = (double)iArrFrequency[i] / iTimesPlayed;
+                statTable[i + 1, PERC] = sPercSpace + dArrPercent[i].ToString("P");
+
+                // if percentage is 100 percent, only 2 tabs after, else 3 tabs
+                if (dArrPercent[i] == 1)   statTable[i + 1, PERC] += "\t\t";
+                else                            statTable[i + 1, PERC] += "\t\t\t";
+
+            }
+        }
+
+        /// <summary>
+        /// updates the game Info box text info
+        /// </summary>
+        private void UpdateGameInfo()
+        {
+            // display updated results in Game Info box
+            amtPlayed.Text = iTimesPlayed.ToString();
+            amtWon.Text = iTimesWon.ToString();
+            amtLost.Text = iTimesLost.ToString();
+        }
+
+        /// <summary>
+        /// updates the game stats in bottome rich text box
+        /// </summary>
+        private void UpdateGameStatsBox()
+        {
+            statsBox.Text = "";
+            for (int i = 0; i < NUM_ROWS; i++)
+            {
+                for(int j=0; j< NUM_COLS; j++)
+                {
+                    statsBox.Text += statTable[i, j];
+                }
+            }
+        }
+
+        /// <summary>
+        /// simulates a dice roll 
+        /// </summary>
+        private void AnimateDice()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // change the dice image randomly 10 times
+                diceImage.Image = Image.FromFile("die" + iRandom.Next(1, 7).ToString() + ".gif");
+
+                // slow down image change time so the change is visible to the user
+                Thread.Sleep(75);
+
+                // refresh the image;
+                diceImage.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Gets the guess from the textbox, stores it in guess var
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // if entry is a num inclusively between 1 and 6, it's valid, hide error message.
+            if (Int32.TryParse(TextBox1.Text, out guess) && (guess > 0 && guess < 7))
+            {
+                isValid = true;
+                HideErrorMessage();
+                return;
+            }
+
+            // if theres nothing in the textbox return
+            if (TextBox1.Text == "") { return; }
+            
+            // otherwise show error message
+            isValid = false;
+            ShowErrorMessage();
+        }
+
+        /// <summary>
+        /// hides the error message from user interface
+        /// </summary>
+        private void HideErrorMessage()
+        {
+            errorMessage.Visible = false;
+        }
+
+        /// <summary>
+        /// displays the error message for invalid input
+        /// </summary>
+        private void ShowErrorMessage()
+        {
+            errorMessage.Visible = true;
+        }
+
+        /// <summary>
+        /// Shows the input needed message for when user tries to roll without entering a guess
+        /// </summary>
+        private void ShowInputNeededMessage()
+        {
+            noInputMessage.Visible = true;
+        }
+
+        /// <summary>
+        /// Hides Input needed message if user makes a guess
+        /// </summary>
+        private void HideInputNeededMessage()
+        {
+            noInputMessage.Visible = false;
+        }
+
+        /// <summary>
+        /// Resets all game stats, labels, and textboxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ResetButton_Click(object sender, EventArgs e) 
+        {
+            //set all attributes to 0 or empty
+            iTimesWon = 0;
+            iTimesLost = 0;
+            iTimesPlayed = 0;
+
+            //set all array values to 0
+            for (int i = 0; i < NUM_DICE; i++)
+            {
+                iArrFrequency[i] = 0;
+                iArrGuesses[i] = 0;
+                dArrPercent[i] = 0.00;
+            }
+
+            //reset statTable multiArr
+            statTable = new string[NUM_ROWS, NUM_COLS]
+            {
+                //spaces before value, tabs after
+                {"FACE\t",         "FREQUENCY\t",       "PERCENT\t",    "NUMBER OF GUESSES\n" },
+                {sFaceSpace+"1"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"2"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"3"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"4"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"5"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0\n" },
+                {sFaceSpace+"6"+sFaceTab, sFreqSpace+"0"+sFreqTab, sPercSpace+"0.00%"+sPercTab, "0" }
+            };
+
+            // hide all error messages
+            HideErrorMessage();
+            HideInputNeededMessage();
+
+            // empty the guess box
+            TextBox1.Text = null;
+
+            // update gameinfo display, and statBox display
+            UpdateGameStatsBox();
+            UpdateGameInfo();
+
+            // reset the dice image to image 6
+            diceImage.Image = Image.FromFile("die6.gif");
+
+            // reset the statBox rtextbox
+            UpdateGameStatsBox();
+            
+        }
+        #endregion
+
+        /// <summary>
+        /// if enter key is pressed while cursor is in the textBox1, the roll_button is activated
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // found this code on stack overflow to make it easier (ie I don't like using a mouse)
+            //https://stackoverflow.com/questions/12318164/enter-key-press-in-c-sharp
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                RollButton_Click(sender, e);
+            }
+        }
+    }
+}
